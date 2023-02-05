9f790678f888d4ee7fbcd4dad9ffde759950ec96
mleger45 <mleger45@gmail.com>
2023-02-04 21:28:40 -0500

    adding top bar

---
+++ src/Layout/Header.tsx
@@ -0,0 +1,108 @@
+import React, {useEffect, useState} from "react";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import { styled, alpha } from '@mui/material/styles';
+import Autocomplete from '@mui/material/Autocomplete';
+import { TextField } from "@mui/material";
+
+
+
+const version = "1.0.0";
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  }));
+  
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+  
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+  }));
+
+ 
+const Header = () => {
+
+    const choices = ["AAPL", "JPM"]
+
+    const searchInputHandler = (data: any) => {
+        console.log("Handler well placed", data.target.value)
+    }
+    
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              TICKERLAND V{version}
+            </Typography>
+            {/* <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search Ticker..."
+              inputProps={{ 'aria-label': 'search' }}
+              onChange = {searchInputHandler}
+            />
+          </Search> */}
+          <Autocomplete
+        id="tickers"
+        freeSolo
+        options={choices}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Searh Ticker" />}
+      />
+
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+  }
+
+  export default Header;
