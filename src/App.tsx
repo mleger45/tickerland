51a457894983687b629887b5a0234546bf328936
mleger45 <mleger45@gmail.com>
2023-02-11 01:20:49 -0500

    first iteration again

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Header from './Layout/Header';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <Header/>
-      <Button variant="contained">Hello World</Button>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
